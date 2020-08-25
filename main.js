6cb8ef181dab6be2a133afbf1b8e3f3d4215d07f
Denis <daz@3390017.ru>
2020-08-25 13:05:51 +0300

    lesson2

--- main.js
+++ main.js
@@ -1,55 +1,87 @@
-const goods = [
-  { title: "Shirt", price: 150 },
-  { title: "Socks", price: 50 },
-  { title: "Jacket", price: 350 },
-  { title: "Shoes", price: 250 },
+let goods = [
+    {title: 'Shirt', price: 150},
+    {title: 'Socks', price: 50},
+    {title: 'Jacket', price: 350},
+    {title: 'Shoes', price: 250},
 ];
 
-const renderGoodsItem = (title = "Товар", price = 100) => {
-  return /*html*/ `
-  <div class="section goods-item">
-        <div class="item">
-            <div class="image">
 
-            </div>
+class GoodsItem {
+    constructor(title, price) {
+        this.title = title;
+        this.price = price;
+    }
 
-            <div class="text">
+    render() {
+        return `
+              <div class="section goods-item">
+                    <div class="item">
+                        <div class="image">
 
-                <h4>Описание: </h4>
-                <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Ad aspernatur assumenda deserunt
-                    dicta
-                    incidunt ipsam laborum porro sed sunt ullam! Ad amet assumenda commodi consequatur dolores
-                    eligendi fugiat minus, necessitatibus obcaecati, odit officia perspiciatis porro quos
-                    voluptas
-                    voluptate! Aperiam assumenda deleniti eum fugit id in, odit optio quaerat totam
-                    veritatis.</p>
-            </div>
-            <div class="buy-block">
-                    <div class="discount"><span>-66.6%</span></div>
-                <div class="bestsell"><span>Надо брать!</span></div>
-                <br>
-                <span class="name"><a
-                        href="{% url 'main:product' item.pk %}">${title}</a></span><br>
-                <div class="price">
-                    <span class="price"> ${price} &#8381;</span>
+                        </div>
 
-                        <span class="old-price"> ${price * 3} &#8381;</span>
+                        <div class="text">
 
-                    <br>
-                    <button class="buy"><a href="{% url 'basket:add' item.id %}">Добавить товар</a></button>
+                            <h4>Описание: </h4>
+                            <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Ad aspernatur assumenda deserunt
+                                dicta
+                                incidunt ipsam laborum porro sed sunt ullam! Ad amet assumenda commodi consequatur dolores
+                                eligendi fugiat minus, necessitatibus obcaecati, odit officia perspiciatis porro quos
+                                voluptas
+                                voluptate! Aperiam assumenda deleniti eum fugit id in, odit optio quaerat totam
+                                veritatis.</p>
+                        </div>
+                        <div class="buy-block">
+                                <div class="discount"><span>-66.6%</span></div>
+                            <div class="bestsell"><span>Надо брать!</span></div>
+                            <br>
+                            <span class="name"><a
+                                    href="#">${this.title}</a></span><br>
+                            <div class="price">
+                                <span class="price"> ${this.price} &#8381;</span>
+
+                                    <span class="old-price"> ${this.price * 3} &#8381;</span>
+
+                                <br>
+                                <button class="buy"><a href="#">Добавить товар</a></button>
+                            </div>
+
+                        </div>
+                    </div>
                 </div>
+        `;
+    }
+}
 
-            </div>
-        </div>
-    </div>
-    `;
-};
 
-const renderGoodsList = (list = goods) => {
-  let goodsList = list.map((item) => renderGoodsItem(item.title, item.price));
-  document.querySelector(".main").innerHTML = goodsList;
-};
+class GoodsList {
+    constructor() {
+        this.goods = [];
+    }
+
+    fetchGoods(goods = []) {
+        this.goods = goods;
+    }
+
+    getTotalCost() {
+        return this.goods.reduce((sum, {price}) => (sum + price), 0);
+    }
+
+    render() {
+        let listHtml = '';
+        this.goods.forEach(({title, price}) => {
+            const goodItem = new GoodsItem(title, price);
+            listHtml += goodItem.render();
+        });
+        document.querySelector('.main').innerHTML = listHtml;
+    }
+}
 
 window.onload = () => {
-  renderGoodsList();
-};
+    const list = new GoodsList();
+    list.fetchGoods(goods);
+    list.render()
+    console.log(list.getTotalCost())
+}
+
+
